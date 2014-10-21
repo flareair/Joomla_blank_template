560cfdef40633618124cdaa1786c607a471936f7
flareair <flareair0@gmail.com>
2014-10-21 15:38:04 +0400

    Added basic template positions

--- includes/params.php
+++ includes/params.php
@@ -1,8 +1,26 @@
 <?php
 defined('_JEXEC') or die;
 
+
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
+
 $app = JFactory::getApplication();
 $doc = JFactory::getDocument();
+
+$sidecols = 0;
+
+if ($this->countModules('side-left')) {
+  $sidecols += 3;
+}
+
+if ($this->countModules('side-right')) {
+  $sidecols += 3;
+}
+
+$sitename = $app->getCfg('sitename');
 
 $doc->addScript('templates/'.$this->template.'/production/production.js');
 $doc->addStyleSheet('templates/'.$this->template.'/production/production.css');
--- index.php
+++ index.php
@@ -11,10 +11,105 @@ include 'includes/params.php';
   <jdoc:include type="head" />
 </head>
 <body>
-  <div class="container">
-    <h1>
-      Hello world!
-    </h1>
+
+
+<!-- Main menu -->
+
+
+  <?php if ($this->countModules('main-menu')) : ?>
+  <div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/"><?php echo $sitename?></a>
+      </div>
+      <div class="collapse navbar-collapse">
+        <jdoc:include type="modules" name="main-menu" />
+      </div><!--/.nav-collapse -->
+    </div>
   </div>
+  <?php endif; ?>
+
+
+
+<!-- body -->
+
+<div class="container">
+
+  <div class="row">
+
+
+    <!-- sideleft column -->
+
+
+    <?php if ($this->countModules('side-left')) : ?>
+    <div class="col-md-3">
+      <jdoc:include type="modules" name="side-left" />
+    </div>
+    <?php endif; ?>
+
+    <!-- Content -->
+
+    <div class="col-md-<?php echo (12 - $sidecols)?>">
+      <!-- content top -->
+
+
+      <?php if ($this->countModules('content-top')) : ?>
+      <div class="content-top">
+        <jdoc:include type="modules" name="content-top" />
+      </div>
+      <?php endif; ?>
+
+      <!-- main content -->
+
+
+      <div class="content-main">
+        <jdoc:include type="component" />
+        <jdoc:include type="message" />
+      </div>
+
+      <!-- content bottom -->
+
+      <?php if ($this->countModules('content-bottom')) : ?>
+      <div class="content-bottom">
+        <jdoc:include type="modules" name="content-bottom" />
+      </div>
+      <?php endif; ?>
+
+
+    </div>
+
+    <!-- sideleft column -->
+
+
+    <?php if ($this->countModules('side-right')) : ?>
+    <div class="col-md-3">
+      <jdoc:include type="modules" name="side-right" />
+    </div>
+    <?php endif; ?>
+
+
+  </div>
+
+</div>
+
+
+
+<!-- Footer -->
+
+
+<div class="footer">
+  <div class="container">
+    <jdoc:include type="modules" name="footer" />
+  </div>
+</div>
+
+
+
 </body>
 </html>
